f37fbd917f7d873aab5d2aa351ac9619cfe1cac9
dr-JT <tsuky19@gmail.com>
2018-04-25 19:54:41 -0400

    d

--- R/descriptives.R
+++ R/descriptives.R
@@ -9,12 +9,14 @@
 descriptives <- function(x){
   x <- tidyr::gather(x, "Variable", "value")
   x <- dplyr::group_by(x, Variable)
-  x <- dplyr::summarise(n = n(),
+  x <- dplyr::summarise(x,
+                        n = n(),
                         Mean = mean(value, na.rm=TRUE),
                         SD = sd(value, na.rm=TRUE),
                         min = min(value, na.rm=TRUE),
                         max = max(value, na.rm=TRUE),
                         '% Missing' = 100*(length(which(is.na(value)))/n()))
   x <- knitr::kable(x, digits=2, format="html", caption="Descriptive Statistics")
-  kable_styling(x)
+  x <- kable_styling(x)
+  return(x)
 }
