667818734b20b327c18f48344935563c0c7a00cd
dr-JT <tsuky19@gmail.com>
2018-04-25 20:04:45 -0400

    d

--- R/cor.plot.R
+++ R/cor.plot.R
@@ -10,8 +10,9 @@
 cor.plot <- function(x, use = "pairwise"){
   table_r <- cor(x, use = use)
   table_p <- corrplot::cor.mtest(x)$p
-  corrplot::corrplot(table_r, method = "color", type="upper", number.cex = .7,
+  plot <- corrplot::corrplot(table_r, method = "color", type="upper", number.cex = .7,
                              tl.cex = .6, addCoef.col = "black", tl.col = "black", tl.srt = 90,
                              p.mat = table_p,  sig.level = .05, insig = "blank", diag = FALSE,
                              number.digits = 2)
+  return(plot)
 }
