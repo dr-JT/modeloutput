01e7b3aafc8521f6e7be422b9213c333049961c4
dr-JT <tsuky19@gmail.com>
2018-04-25 20:02:10 -0400

    create funciton to create a correltion heat map

---
+++ R/cor.plot.R
@@ -0,0 +1,18 @@
+#' A Results Output Function
+#'
+#' This function will display a Correlation Plot heat map
+#' @param x dataframe
+#' @param use How to deal with missing data. Pairwise or listwise deletion (default = "pairwise")
+#' @export
+#' @examples
+#' cor.plot(x)
+
+cor.plot <- function(x, use = "pairwise"){
+  table_r <- cor(x, use = use)
+  table_p <- corrplot::cor.mtest(x)$p
+  plot <- corrplot::corrplot(table_r, method = "color", type="upper", number.cex = .7,
+                             tl.cex = .6, addCoef.col = "black", tl.col = "black", tl.srt = 90,
+                             p.mat = table_p,  sig.level = .05, insig = "blank", diag = FALSE,
+                             number.digits = 2)
+  return(plot)
+}
