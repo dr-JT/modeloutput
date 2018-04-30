ceb9b9c63804dc14d5bb0d54cfe0c7a785e1ae04
dr-JT <tsuky19@gmail.com>
2018-04-30 13:13:31 -0400

    ,

--- R/hlm.output.R
+++ R/hlm.output.R
@@ -17,7 +17,7 @@ lmer.output <- function(x, random_effects = "Subject"){
   n <- knitr::kable(n, digits=3, format="html")
   n <- kableExtra::kable_styling(n, full_width=FALSE, position="left")
   random <- as.data.frame(VarCorr(x))
-  colnames(random) <- c("Group", "Variable 1", "Variable 2", "Variance", "Std Deviation")
+  colnames(random) <- c("Group", "Variable1", "Variable2", "Variance", "StdDeviation")
   random <- knitr::kable(random, digits=3, format="html")
   random <- kableExtra::kable_styling(random, full_width=FALSE, position="left")
   fixed <- as.data.frame(x.summary$coefficients)
