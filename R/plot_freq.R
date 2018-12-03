4eb03a4bc0735b7ed1a3fc0e0d88ea8100858096
dr-JT <tsuky19@gmail.com>
2018-12-03 17:14:21 -0500

    add sjPlot import

--- R/plot_freq.R
+++ R/plot_freq.R
@@ -7,21 +7,21 @@
 #' plot_freq(data)
 
 plot_freq <- function(x){
-  set_theme(geom.outline.color = "black", geom.outline.size = .15)
+  sjPlot::set_theme(geom.outline.color = "black", geom.outline.size = .15)
   variableList <- colnames(x)
   i <- 1
   p <- list()
   for (variable in variableList){
     if (range(x[[variable]], na.rm = TRUE)[1]<1 & range(x[[variable]], na.rm = TRUE)[1]>=0 & range(x[[variable]], na.rm = TRUE)[2]<=1 & range(x[[variable]], na.rm = TRUE)[2]>0) {
       data.hist <- x[[variable]]
-      p[[i]] <- sjp.frq(data.hist,
+      p[[i]] <- sjPlot::sjp.frq(data.hist,
                         type = "hist",
                         geom.size = .10,
                         axis.title = variable,
                         xlim = c(0,1))
     } else {
       data.hist <- x[[variable]]
-      p[[i]] <- sjp.frq(data.hist,
+      p[[i]] <- sjPlot::sjp.frq(data.hist,
                         type = "hist",
                         axis.title = variable,
                         xlim = c(min(x[[variable]], na.rm = TRUE), max(x[[variable]], na.rm = TRUE)))
