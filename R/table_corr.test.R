258dc806bdf2d6dfd4b23c26ce21372b7f67fd5b
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2019-01-17 19:55:07 -0500

    add values on diagonal

--- R/table_corr.test.R
+++ R/table_corr.test.R
@@ -14,9 +14,6 @@ table_corr.test <- function(x){
   table <- data.frame(Variable = rep(rownames(r), each = 3), parameter = rep(c("r", "n", "p"), nrow(r)))
 
   for (name in rownames(r)){
-    r[name,][name] <- NA
-    n[name,][name] <- NA
-    p[name,][name] <- NA
     
     p[which(lower.tri(p))] <- p[which(upper.tri(p))]
 
@@ -25,7 +22,6 @@ table_corr.test <- function(x){
       table[which(table$parameter=="n" & table$Variable==pair), name] <- round(n[pair,name], 2)
       table[which(table$parameter=="p" & table$Variable==pair), name] <- round(p[pair,name], 2)
     }
-    table[which(is.na(table[,name])),name] <- ""
   }
 
   table <- knitr::kable(table, digits=2, format="html", caption="Correlations")
