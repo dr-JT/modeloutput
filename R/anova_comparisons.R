5f647d14adc356a7cd52cefd574bd3215618e848
Tsukahara <jtsukahara3@ipsec-10-2-192-179.vpn.gatech.edu>
2022-07-08 20:38:21 -0400

    update

--- R/anova_comparisons.R
+++ R/anova_comparisons.R
@@ -3,14 +3,29 @@
 #' @param x an lmer model object
 #' @param term The factor at which to compare levels at
 #' @param digits How many decimal places to round to? Default is 3.
+#' @param pbkrtest.limit Optional parameter that can be set to help calculate dfs.
+#'     If you need to use this a warning message will appear in the console
+#'     telling you what to set this at.
+#' @param lmerTest.limit Optional parameter that can be set to help calculate dfs.
+#'     If you need to use this a warning message will appear in the console
+#'     telling you what to set this at.
 #' @export
 #'
 
-anova_comparisons <- function(x, term = NULL, digits = 3) {
+anova_comparisons <- function(x, term = NULL, digits = 3,
+                              pbkrtest.limit = NULL,
+                              lmerTest.limit = NULL) {
 
   term_levels <- levels(insight::get_data(x)[[term]])
 
-  table <- modelbased::estimate_contrasts(x, contrast = term)
+  if (is.null(pbkrtest.limit) & is.null(lmerTest.limit)) {
+    table <- modelbased::estimate_contrasts(x, contrast = term)
+  } else {
+    table <- modelbased::estimate_contrasts(x, contrast = term,
+                                            pbkrtest.limit = pbkrtest.limit,
+                                            lmerTest.limit = lmerTest.limit)
+  }
+
   table_std <- effectsize::standardize(table)
   table_std <- dplyr::select(table_std, Level1, Level2, Cohen_D = Difference)
 
