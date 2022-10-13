af5c574aa1812645283765a144798fe9903a442f
Tsukahara <jtsukahara3@ipsec-10-2-194-151.vpn.gatech.edu>
2022-10-13 09:18:21 -0400

    fix effectsize estimates

--- R/anova_modelsig.R
+++ R/anova_modelsig.R
@@ -1,12 +1,22 @@
 #' ANOVA F-Table for Overall Main Effect and Interaction Terms
 #'
 #' @param x an lmer model object
+#' @param eta_squared logical. Include partial-eta sqaured effect size?
+#'     Default: TRUE
+#' @param omega_squared logical. Include omega sqaured effect size?
+#'     Default: TRUE
+#' @param epsilon_squared logical. Include epsilon sqaured effect size?
+#'     Default: FALSE
 #' @param digits How many decimal places to round to? Default is 3.
 #' @param id_col The column containing subject ids. Default is "Subject"
 #' @export
 #'
 
-anova_modelsig <- function(x, digits = 3, id_col = "Subject") {
+anova_modelsig <- function(x,
+                           eta_squared = TRUE,
+                           omega_squared = TRUE,
+                           epsilon_squared = FALSE,
+                           digits = 3, id_col = "Subject") {
 
   format_table <- function(x, digits = digits) {
     x <- dplyr::rename(x, Term = Parameter, Sum_of_Squares = Sum_Squares)
@@ -19,6 +29,16 @@ anova_modelsig <- function(x, digits = 3, id_col = "Subject") {
     x <- dplyr::mutate(x,
                        p = round(p, 3))
     return(x)
+  }
+  effectsize_list <- c()
+  if (eta_squared == TRUE) {
+    effectsize_list <- c(effectsize_list, "eta")
+  }
+  if (omega_squared == TRUE) {
+    effectsize_list <- c(effectsize_list, "omega")
+  }
+  if (epsilon_squared == TRUE) {
+    effectsize_list <- c(effectsize_list, "epsilon")
   }
 
   model_type <- insight::model_name(x)
@@ -43,9 +63,7 @@ anova_modelsig <- function(x, digits = 3, id_col = "Subject") {
   dv <- insight::find_response(x)
 
   table <- parameters::model_parameters(x_parameters, type = 3,
-                                        eta_squared = TRUE,
-                                        omega_squared = TRUE,
-                                        epsilon_squared = TRUE)
+                                        effectsize_type = effectsize_list)
   table <- as.data.frame(table)
   table <- dplyr::mutate(table, Mean_Square_Error = Mean_Square / `F`)
   if (stringr::str_detect(model_type, "lmer")) {
