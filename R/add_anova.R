e438e7ccb8e9e4d59c228bc9311d37f8df4c546f
Tsukahara <jtsukahara3@ipsec-10-2-225-191.vpn.gatech.edu>
2023-04-27 14:15:56 -0400

    add anova

---
+++ R/add_anova.R
@@ -0,0 +1,12 @@
+#' Insert ANOVA Section
+#'
+#' @export
+#'
+
+add_anova <- function() {
+  anova_url <- "https://raw.githubusercontent.com/dr-JT/modeloutput/main/inst/extdata/_extensions/anova.qmd"
+
+  # Make a GET request to retrieve the raw content of the file
+  anova_text <- httr::content(httr::GET(anova_url), as = "text")
+  writeLines(anova_text)
+}
