8ca1018cf55533fe11b45a1405260f1b79aa2041
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-04-26 21:19:18 -0400

    addin and new function

---
+++ R/new_document.R
@@ -0,0 +1,23 @@
+#' Create New Quarto Document
+#'
+#' @export
+#'
+
+# `file_name` will be the name of the sub-directory and new qmd report within it
+# `ext_name` needs to name the specific extension
+
+new_document <- function() {
+  yaml_url <- "https://raw.githubusercontent.com/dr-JT/modeloutput/main/inst/extdata/_extensions/yaml.qmd"
+  setup_url <- "https://raw.githubusercontent.com/dr-JT/modeloutput/main/inst/extdata/_extensions/setup.qmd"
+  data_url <- "https://raw.githubusercontent.com/dr-JT/modeloutput/main/inst/extdata/_extensions/data.qmd"
+  foot_url <- "https://raw.githubusercontent.com/dr-JT/modeloutput/main/inst/extdata/_extensions/footer.qmd"
+
+  # Make a GET request to retrieve the raw content of the file
+  yaml_text <- httr::content(httr::GET(yaml_url), as = "text")
+  setup_text <- httr::content(httr::GET(setup_url), as = "text")
+  data_text <- httr::content(httr::GET(data_url), as = "text")
+  footer_text <- httr::content(httr::GET(foot_url), as = "text")
+
+  rstudioapi::documentNew(c(yaml_text, setup_text, data_text, footer_text),
+                          type = "rmarkdown")
+}
