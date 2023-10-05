be0a7fe7d70baca67951e76bf21cf2b7f188494b
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-10-05 00:08:45 -0400

    fix

--- R/table_styling.R
+++ R/table_styling.R
@@ -32,7 +32,7 @@ table_styling <- function(x) {
         )
       ),
       locations = gt::cells_column_labels(
-        columns = dplyr::any_of(c(Model, Term))
+        columns = dplyr::any_of(c("Model", "Term"))
       )
     ) |>
     gt::opt_horizontal_padding(scale = 2)
