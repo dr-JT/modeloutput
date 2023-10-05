45cdf31964f1798cc70047ca564a6d5a1ce3c4c9
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-10-05 00:24:34 -0400

    f

--- R/regression_coeff.R
+++ R/regression_coeff.R
@@ -113,7 +113,6 @@ regression_coeff <- function(x, y = NULL, z = NULL,
 
     if (unstandardized == TRUE & standardized == FALSE) {
       table <- table |>
-        gt::cols_hide(columns = c(B, SE_B, CI_std)) |>
         gt::tab_spanner(label = "Unstandardized",
                         columns = c(b, SE, CI_unstd)) |>
         gt::cols_label(CI_unstd = "95% CI")
