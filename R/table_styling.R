f52cde9dfc3b18354b337623636a234cf867ac87
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-10-09 14:34:17 -0400

    update style

--- R/table_styling.R
+++ R/table_styling.R
@@ -8,35 +8,22 @@ table_styling <- function(x) {
   x <- x |>
     gt::tab_options(
       table.align = "left",
-      heading.border.bottom.color = "black",
-      heading.title.font.size = 12,
-      heading.align = "center",
-      table.border.top.color = "black",
-      heading.title.font.weight = "bolder",
-      column_labels.font.weight = "bold",
-      footnotes.font.size = 12,
       table.border.top.width = 0,
       table.border.bottom.width = 0,
-      column_labels.border.top.width = 2,
-      column_labels.border.top.color = "black",
-      column_labels.border.bottom.width = 0,
-      column_labels.border.bottom.color = "black",
-      table_body.border.bottom.color = "black",
-      table_body.border.top.color = "black"
+      heading.align = "left",
+      heading.padding = 10,
+      heading.border.bottom.width = 0,
+      heading.title.font.size = 18,
+      heading.title.font.weight = "bolder",
+      column_labels.border.top.width = 0,
+      column_labels.font.weight = "bold",
+      column_labels.padding = 6,
+      footnotes.font.size = 14
     ) |>
-    gt::tab_style(
-      style = list(
-        gt::cell_borders(
-          sides = c("top", "bottom")
-        ),
-        gt::cell_text(
-          align = "center"
-        )
-      ),
-      locations = gt::cells_column_labels(
-        columns = -dplyr::any_of(c("Model", "Term"))
-      )
-    ) |>
-    gt::opt_horizontal_padding(scale = 2)
+    gt::tab_style(style = gt::cell_text(align = 'center'),
+                  locations = gt::cells_column_labels()) |>
+    gt::opt_horizontal_padding(scale = 3) |>
+    gt::cols_align_decimal(columns = dplyr::everything()) |>
+    gt::sub_missing(missing_text = "")
   return(x)
 }
