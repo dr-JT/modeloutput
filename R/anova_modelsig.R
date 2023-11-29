7b73404276ed7dd30ff3ce3d0d0c8d464fcf8df8
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-11-29 09:48:45 -0500

    update style

--- R/anova_modelsig.R
+++ R/anova_modelsig.R
@@ -130,16 +130,14 @@ anova_modelsig <- function(x,
     gt::fmt_number(decimals = digits, use_seps = FALSE) |>
     gt::tab_footnote(paste("Model: ", add_fun_name,
                            deparse1(x_formula), add_parenth, sep = "")) |>
+    gt::tab_footnote(gt_table,
+                     paste("df correction: ", df_correction, sep = "")) |>
     gt::tab_footnote(paste("N = ", x_n, sep = ""))
 
   if (stringr::str_detect(model_type, "lmer")) {
     gt_table <- gt::tab_footnote(gt_table,
                                  paste("Observations = ", x_obs, sep = ""))
   }
-
-  gt_table <- gt::tab_footnote(gt_table,
-                               paste("df correction: ",
-                                     df_correction, sep = ""))
 
   return(gt_table)
 }
