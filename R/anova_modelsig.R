fb6875b7958f034c00392c1bb3307e180428bd9f
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2025-03-10 12:55:40 -0400

    Update anova_modelsig.R

--- R/anova_modelsig.R
+++ R/anova_modelsig.R
@@ -87,10 +87,16 @@ anova_modelsig <- function(x,
                    Mean_Square = "MS")
 
   if (ws_design == TRUE) {
-    gt_table <- gt::cols_label(gt_table,
+    if (model_type != "lmer") {
+      gt_table <- gt::cols_label(gt_table,
                                Sum_Squares_Error = "SS Error",
                                df_error = "df Error",
                                Mean_Square_Error = "MS Error")
+    }
+    if (model_type == "lmer") {
+      gt_table <- gt::cols_label(gt_table,
+                               df_error = "df Error")
+    }
   }
 
   if (eta_squared == TRUE) {
